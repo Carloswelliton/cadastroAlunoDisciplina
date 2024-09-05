834cd7a29c539f75597356a2e9c940c7d11c1ccd
Carlos Welliton <carloswelliton7@gmail.com>
2024-09-05 15:02:03 -0300

    configurando data completa do salvamento dos formulários

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 var path = require('path');
-    
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -23,20 +23,20 @@ app.get("/cadastrar/disciplina", (req, res)=>{
 
 
 app.post("/salvar/aluno", (req, res) => {
-    const erros = [];
+    const data = new Date();
 
     const aluno = {
         nome: req.body.nome,
-        //nascimento: req.body.nascimento,
         matricula: req.body.matricula,
-        turma: req.body.turma
+        turma: req.body.turma,
+        data: data
     }
     const formatarData = (dataISO) => {
         const [ano, mes, dia] = dataISO.split("-");
         return `${dia}/${mes}/${ano}`;
     }
-
     const dataFormatada = formatarData(req.body.nascimento);
+    
     const dataNascimento = {
         novaData: dataFormatada
     }
@@ -49,11 +49,13 @@ app.post("/salvar/aluno", (req, res) => {
 
 app.post("/salvar/disciplina", (req, res) => {
 
+    const data = new Date();
     const disciplina = {
         disciplina: req.body.nomeDisciplina,
         professor: req.body.professor,
         codigo: req.body.codigo,
-        turmaCodigo: req.body.turmaCodigo 
+        turmaCodigo: req.body.turmaCodigo,
+        data: data
     }
 
     res.render("salvarDisciplina", {    
